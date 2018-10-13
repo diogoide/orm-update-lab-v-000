a67c3fd38dd46e552f89038ec0b55624391f7985
Diogo Ide <diogoide@gmail.com>
2018-10-13 23:53:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,7 +46,6 @@ class Student
 
      def self.create(name, grade)
       new_student = self.new
-      new_student.id = row[0]
       new_student.name = row[1]
       new_student.grade = row[2]
       new_student

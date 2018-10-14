ece505566396420253359da1312053bdb6599731
Diogo Ide <diogoide@gmail.com>
2018-10-14 00:13:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,7 +52,7 @@ class Student
     end
 
     def self.new_from_db(row)
-      new_student = self.new(row[1],row[2],row[3])
+      new_student = self.new(row[0],row[1],row[2])
       new_student
     end
 

b5a1e3a70dcbd2408bb082f9e42dbda0015e7d22
Diogo Ide <diogoide@gmail.com>
2018-10-14 00:02:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,7 +28,7 @@ class Student
    def self.drop_table
      sql = <<-SQL
      DROP TABLE students
-     SQL 
+     SQL
      DB[:conn].execute(sql)
    end
 
@@ -39,9 +39,9 @@ class Student
        sql = <<-SQL
         INSERT INTO students (name, grade)
         VALUES (?, ?)
-       SQL
-       DB[:conn].execute(sql, self.name, self.grade)
-       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students") [0] [0]
+        SQL
+        DB[:conn].execute(sql, self.name, self.grade)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students") [0] [0]
      end
    end
 
